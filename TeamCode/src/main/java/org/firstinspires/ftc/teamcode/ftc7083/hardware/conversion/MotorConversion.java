3221420c54abfb0531d6fde65af0aaa43965a6ba
Roy Brabson <rbrabson@gmail.com>
2024-09-07 15:40:38 -0400

    unit conversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/hardware/conversion/MotorConversion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/hardware/conversion/MotorConversion.java
@@ -10,18 +10,18 @@ public class MotorConversion {
 
     public MotorConversion(double ticksPerRev, double gearRatio) {
         double ticksPer360 = ticksPerRev * gearRatio;
-        ticksPerDegree = new BigDecimal(ticksPer360).divide(DEGREES_360, MathContext.UNLIMITED);
+        ticksPerDegree = new BigDecimal(ticksPer360).divide(DEGREES_360, MathContext.DECIMAL128);
     }
 
     public double positionToDegrees(double position) {
         BigDecimal pos = new BigDecimal(position);
-        BigDecimal degrees = pos.multiply(ticksPerDegree);
+        BigDecimal degrees = pos.divide(ticksPerDegree, MathContext.DECIMAL128);
         return degrees.doubleValue();
     }
 
     public double degreesToPosition(double degrees) {
         BigDecimal deg = new BigDecimal(degrees);
-        BigDecimal position = deg.divide(ticksPerDegree, MathContext.UNLIMITED);
+        BigDecimal position = deg.multiply(ticksPerDegree);
         return position.doubleValue();
     }
 }
