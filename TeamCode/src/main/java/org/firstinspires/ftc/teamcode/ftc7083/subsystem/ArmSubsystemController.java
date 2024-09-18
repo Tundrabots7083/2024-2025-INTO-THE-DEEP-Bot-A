fb1a828560cf1ee1b9028ff1b1220eea6e029e0f
Jonathan Serb <jonathanserb@gmail.com>
2024-09-18 18:44:35 -0400

    Added getter methods for pitch and yaw. Refined comments. Changed setting methods to void as there are now getter methods. Removed rollServo from the initialization list.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/ArmSubsystemController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/ArmSubsystemController.java
@@ -3,12 +3,10 @@ package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
 public class ArmSubsystemController  {
     Wrist wrist;
 
-    ArmSubsystemController(Wrist wrist){
-        wrist = wrist;
-    }
+    ArmSubsystemController(Wrist wrist){ wrist = wrist; }
 
     public void liftArm(double angle){
         //shoulder.rotate(angle);
-       double currentPitch = wrist.setPitch(180 - angle);
+        wrist.setPitch(180 - angle);
     }
 }
