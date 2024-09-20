f4c8b4cea6b0a8bfa5b0257f22eb5ec767e92213
Sarada <sarada43@yahoo.com>
2024-09-19 20:37:04 -0400

    Add Linear Slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/LinearSlide.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.ftc7083.hardware.Motor;
@@ -11,7 +13,6 @@ import org.firstinspires.ftc.teamcode.ftc7083.hardware.Motor;
 
 public class LinearSlide extends SubsystemBase {
     private final Motor slideMotor;
-    private final Motor sholderMotor;
     private final Telemetry telemetry;
 
     /**
@@ -23,27 +24,42 @@ public class LinearSlide extends SubsystemBase {
     public LinearSlide(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         slideMotor = new Motor(hardwareMap, telemetry, "armSlideMotor");
-        sholderMotor = new Motor(hardwareMap, telemetry, "armSholderMotor");
+        configMotor(slideMotor);
     }
 
-    /**
-     * Sets the sholder motor to a position in degrees.
-     *
-     * @param angle Angle of desired arm position in degrees
-     */
-    public void setSholderAngle(double angle) {
-        sholderMotor.setDegrees(angle);
-
-    }
 
     /**
      * Sets the slide length to a length in inches.
      *
      * @param length Length of desired slide position in inches.
      */
-    public void setSlideLength(double length) {
+    public void setLength(double length) {
         slideMotor.setInches(length);
 
+    }
+
+    /**
+     * Gets the slide length in inches
+     * Finds the value for the length
+     *
+     * @return slide length in inches
+     */
+    public double getLength() {
+        return slideMotor.getInches();
+    }
+
+    /**
+     * Configures the motor used for the linear slide
+     *
+     * @param motor
+     */
+    private void configMotor(Motor motor) {
+        MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
+        motorConfigurationType.setTicksPerRev(1.0);
+        motorConfigurationType.setGearing(16.0);
+        motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
+        motor.setMotorType(motorConfigurationType);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
 }
