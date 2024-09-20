bf9634a6bf2178c0e0f62b3bf92ecca8d2872a9a
Sarada <sarada43@yahoo.com>
2024-09-19 20:37:04 -0400

    Add Linear Slide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/opmode/test/LinearSlideTest.java
@@ -0,0 +1,35 @@
+
+package org.firstinspires.ftc.teamcode.ftc7083.opmode.test;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.ftc7083.subsystem.LinearSlide;
+
+@Config
+@TeleOp(name = "Linear Slide Test", group = "tests")
+public class LinearSlideTest extends OpMode {
+    public static double LINEAR_SLIDE_LENGTH = 0.0;
+    private LinearSlide linearSlide;
+
+    @Override
+    public void init() {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        linearSlide = new LinearSlide(hardwareMap, telemetry);
+
+        telemetry.addLine("Initialization Complete");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop() {
+        linearSlide.setLength(LINEAR_SLIDE_LENGTH);
+        telemetry.addData("Target Length", LINEAR_SLIDE_LENGTH);
+        telemetry.addData("Current Length", linearSlide.getLength());
+    }
+
+}
