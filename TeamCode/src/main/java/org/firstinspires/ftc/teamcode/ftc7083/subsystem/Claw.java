1cb708a735dd284c17381ee8f2360f24ebba5aa2
OviesBrabson <oviesbrabson@gmail.com>
2024-09-22 20:44:50 -0400

    Modified ClawTest so that all claw methods can be tested using FTC Dashboard.  Added comments and made minor code changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Claw.java
@@ -1,50 +1,59 @@
 package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.ftc7083.hardware.Servo;
 
 /**
- * The Claw class implements a claw used by the robot to pick up samples
- * and specimens.  By default the claw is closed initially.  Users can request that
- * the claw be opened to some angle specified in degrees.  The claw is reset to its
- * initial closed position when a close is requested.
+ * The Claw class implements a claw used by the robot to pick up and score
+ * samples and specimens.  By default the claw is closed or set to its zero
+ * position initially.  Users can request that the claw be opened or moved
+ * from its zero position to some angle specified in degrees.  The claw is
+ * reset to its zero position when a close is requested.
  */
 @Config
 public class Claw extends SubsystemBase {
-    // Make default settable by FTC dashboard
+    // Make default open/close degrees settable by FTC dashboard
     public static double DEFAULT_OPEN_DEGREES = 45.0;
+    public static double DEFAULT_CLOSE_DEGREES = 0.0;
+    // Make max claw degrees settable by FTC dashboard
+    public static double MAX_CLAW_DEGREES = 270.0;
+    // Implement the claw using a Servo class
     private final Servo servo;
 
     /**
      * Constructor
      *
-     * @param hardwareMap  Servo configuration information
-     * @param deviceName   Name of servo configuration information -- must match name configured
-     *                     in Control Hub or Expansion Hub
-     * @param maxDegrees   Maximum number of degrees that the claw servo can be opened to.
+     * @param hardwareMap Servo configuration information
+     * @param deviceName  Name of servo configuration information -- must match name configured
+     *                    in Control Hub or Expansion Hub
+     * @param maxDegrees  Maximum number of degrees that the claw servo can be opened to.
      */
     public Claw(HardwareMap hardwareMap, String deviceName, double maxDegrees) {
         servo = new Servo(hardwareMap, deviceName);
+        servo.setDirection(com.qualcomm.robotcore.hardware.Servo.Direction.FORWARD);
         servo.setMaxDegrees(maxDegrees);
-        // todo: May want to set initial position and direction for servo.
+        servo.setDegrees(DEFAULT_CLOSE_DEGREES);
     }
 
     /**
-     * Opens claw to a default angle of 45 degrees.
+     * Opens claw to a default angle of 45 degrees from its zero position.
      *
-     * @return  double:  The degrees the claw was opened to.
+     * @return double:  The degrees the claw was opened to.
+     * Should be 45.
      */
     public double open() {
-        servo.setDegrees(DEFAULT_OPEN_DEGREES);
-        return servo.getDegrees();
+        return open(DEFAULT_OPEN_DEGREES);
     }
 
     /**
-     * Opens claw to an angle of the requested degrees
+     * Opens the claw the number of degrees requested from its zero
+     * position.
      *
-     * @param degrees  Size of the angle the claw is to be opened to.
-     * @return  double:  The angle in degrees that the claw was opened to.
+     * @param degrees: Number of degrees the claw is to be opened from
+     *                 its zero position.
+     * @return double:  The angle in degrees that the claw was opened to.
      */
     public double open(double degrees) {
         servo.setDegrees(degrees);
@@ -52,22 +61,23 @@ public class Claw extends SubsystemBase {
     }
 
     /**
-     * Creates a method of getting the degrees
-     * @return: returns the angle in degrees
+     * Get the current position of the claw in degrees from
+     * it's start position.
+     *
+     * @return double: The current position of the claw in degrees.
      */
-    public double getDegrees() {
+    public double getCurrentPosition() {
         return servo.getDegrees();
     }
+
     /**
-     * Totally close the claw back to 0 degrees.
+     * Close the claw back to its zero position.
      *
-     * @return double:  angle in degrees that claw was set to
+     * @return double:  Degrees from claw's 0 position.
+     * Should be zero.
      */
     public double close() {
-        servo.setDegrees(0);
+        servo.setDegrees(DEFAULT_CLOSE_DEGREES);
         return servo.getDegrees();
     }
-
-
-
 }
