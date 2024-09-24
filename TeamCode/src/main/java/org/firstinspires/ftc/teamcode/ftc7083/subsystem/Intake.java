2ee3380df4ede602fd69d4372de9c91fb9372647
jonathanserb <jonathanserb@gmail.com>
2024-09-24 19:52:44 -0400

    Added getPose2d and feedforward methods to ArmSubsystemController. Added comments.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Intake.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
+
+/**
+ * Arm
+ * Slide
+ * Wrist
+ * Claw
+ * Interact between them here.
+ *
+ *Pick up sample
+ * Pick up specimen
+ *
+ * Extend arm
+ * Retract arm
+ * Raise arm
+ * Lower arm
+ */
+public class Intake extends SubsystemBase {
+
+
+}
