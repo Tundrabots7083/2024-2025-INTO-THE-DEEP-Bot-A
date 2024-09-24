37f30b48f14c52d454e046be6c9a4497c775d169
Jacob <jacobcantrell@outlook.com>
2024-09-23 20:02:59 -0400

    Merge remote-tracking branch 'origin/arm' into arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Arm.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.ftc7083.feedback.PIDController;
 import org.firstinspires.ftc.teamcode.ftc7083.hardware.Motor;
 
 /**
@@ -12,9 +14,15 @@ import org.firstinspires.ftc.teamcode.ftc7083.hardware.Motor;
  */
 
 public class Arm extends SubsystemBase {
+    public static double kP;
+    public static double kI;
+    public static double kD;
+    public static double TOLERABLE_ERROR = 1.0;
+
     private final Motor shoulderMotor;
     private final Telemetry telemetry;
-
+    public PIDController pidController;
+    private double angle = 0.0;
     /**
      * Makes an arm that can raise and lower.
      *
@@ -24,6 +32,8 @@ public class Arm extends SubsystemBase {
     public Arm(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         shoulderMotor = new Motor(hardwareMap, telemetry, "armShoulderMotor");
+        configMotor(shoulderMotor);
+        pidController = new PIDController(kP, kI, kD);
     }
 
     /**
@@ -31,24 +41,46 @@ public class Arm extends SubsystemBase {
      *
      */
     public void setShoulderAngle(double angle) {
-        shoulderMotor.setDegrees(angle);
+        this.angle = angle;
+        pidController.reset();
     }
 
     /**
      * Gets the shoulder motor position to
-     * @return
      */
     public double getShoulderAngle() {
         return shoulderMotor.getDegrees();
     }
 
+    /**
+     * Configures the shoulder motor.
+     * @param motor
+     */
     private void configMotor(Motor motor) {
         MotorConfigurationType motorConfigurationType = motor.getMotorType().clone();
         motorConfigurationType.setTicksPerRev(1.0);
         motorConfigurationType.setGearing(16.0);
         motorConfigurationType.setAchieveableMaxRPMFraction(1.0);
         motor.setMotorType(motorConfigurationType);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setMode(Motor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(Motor.RunMode.RUN_USING_ENCODER);
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
+    /**
+     * Sends power to the PID controller.
+     */
+    public void execute() {
+        double power = pidController.calculate(angle, shoulderMotor.getDegrees());
+        shoulderMotor.setPower(power);
+    }
+
+    /**
+     * Can tell if the current position is within a reasonable distance of the target.
+     * @return
+     */
+    public boolean isAtTarget(){
+        double error = Math.abs(angle - shoulderMotor.getDegrees());
+        return error <= TOLERABLE_ERROR;
+    }
 }
