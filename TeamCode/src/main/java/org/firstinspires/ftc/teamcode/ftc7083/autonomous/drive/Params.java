10342674e97081f46f0fe8dbb8cfd12320933061
Roy Brabson <rbrabson@gmail.com>
2024-10-26 00:39:21 -0400

    use sparkfun roadruner fork

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/drive/Params.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/drive/Params.java
@@ -1,34 +1,44 @@
 package org.firstinspires.ftc.teamcode.ftc7083.autonomous.drive;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 
 @Config
 public class Params {
-    // drive model parameters
-    public static double inPerTick = 1;
-    public static double lateralInPerTick = inPerTick;
-    public static double trackWidthTicks = 0;
+    // Assuming you've mounted your sensor to a robot and it's not centered,
+    // you can specify the offset for the sensor relative to the center of the
+    // robot. The units default to inches and degrees, but if you want to use
+    // different units, specify them before setting the offset! Note that as of
+    // firmware version 1.0, these values will be lost after a power cycle, so
+    // you will need to set them each time you power up the sensor. For example, if
+    // the sensor is mounted 5 inches to the left (negative X) and 10 inches
+    // forward (positive Y) of the center of the robot, and mounted 90 degrees
+    // clockwise (negative rotation) from the robot's orientation, the offset
+    // would be {-5, 10, -90}. These can be any value, even the angle can be
+    // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
 
-    // feedforward parameters (in tick units)
-    public static double kS = 0;
-    public static double kV = 0;
-    public static double kA = 0;
+    // RR localizer note: These units are inches and radians.
+    public static double SPARKFUN_OTOS_OFFSET_X = 0.0;
+    public static double SPARKFUN_OTOS_OFFSET_Y = 0.0;
+    public static double SPARKFUN_OTOS_HEADING_IN_DEGREES = 0.0;
+    public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(SPARKFUN_OTOS_OFFSET_X, SPARKFUN_OTOS_OFFSET_Y, Math.toRadians(SPARKFUN_OTOS_HEADING_IN_DEGREES));
 
-    // path profile parameters (in inches)
-    public static double maxWheelVel = 50;
-    public static double minProfileAccel = -30;
-    public static double maxProfileAccel = 50;
-
-    // turn profile parameters (in radians)
-    public static double maxAngVel = Math.PI; // shared with path
-    public static double maxAngAccel = Math.PI;
-
-    // path controller gains
-    public static double axialGain = 0.0;
-    public static double lateralGain = 0.0;
-    public static double headingGain = 0.0; // shared with turn
-
-    public static double axialVelGain = 0.0;
-    public static double lateralVelGain = 0.0;
-    public static double headingVelGain = 0.0; // shared with turn
+    // Here we can set the linear and angular scalars, which can compensate for
+    // scaling issues with the sensor measurements. Note that as of firmware
+    // version 1.0, these values will be lost after a power cycle, so you will
+    // need to set them each time you power up the sensor. They can be any value
+    // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
+    // first set both scalars to 1.0, then calibrate the angular scalar, then
+    // the linear scalar. To calibrate the angular scalar, spin the robot by
+    // multiple rotations (eg. 10) to get a precise error, then set the scalar
+    // to the inverse of the error. Remember that the angle wraps from -180 to
+    // 180 degrees, so for example, if after 10 rotations counterclockwise
+    // (positive rotation), the sensor reports -15 degrees, the required scalar
+    // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
+    // robot a known distance and measure the error; do this multiple times at
+    // multiple speeds to get an average, then set the linear scalar to the
+    // inverse of the error. For example, if you move the robot 100 inches and
+    // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
+    public double linearScalar = 1.0;
+    public double angularScalar = 1.0;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/drive/SparkFunOTOSDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/autonomous/drive/SparkFunOTOSDrive.java
@@ -11,6 +11,7 @@ import com.acmerobotics.roadrunner.ftc.FlightRecorder;
 import com.acmerobotics.roadrunner.ftc.SparkFunOTOSCorrected;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.roadrunner.messages.PoseMessage;
@@ -23,52 +24,15 @@ import org.firstinspires.ftc.teamcode.roadrunner.messages.PoseMessage;
  * Unless otherwise noted, comments are from SparkFun
  */
 public class SparkFunOTOSDrive extends AutoMecanumDrive {
-    public static class Params {
-        // Assuming you've mounted your sensor to a robot and it's not centered,
-        // you can specify the offset for the sensor relative to the center of the
-        // robot. The units default to inches and degrees, but if you want to use
-        // different units, specify them before setting the offset! Note that as of
-        // firmware version 1.0, these values will be lost after a power cycle, so
-        // you will need to set them each time you power up the sensor. For example, if
-        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
-        // forward (positive Y) of the center of the robot, and mounted 90 degrees
-        // clockwise (negative rotation) from the robot's orientation, the offset
-        // would be {-5, 10, -90}. These can be any value, even the angle can be
-        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
-
-        // RR localizer note: These units are inches and radians.
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
-
-        // Here we can set the linear and angular scalars, which can compensate for
-        // scaling issues with the sensor measurements. Note that as of firmware
-        // version 1.0, these values will be lost after a power cycle, so you will
-        // need to set them each time you power up the sensor. They can be any value
-        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
-        // first set both scalars to 1.0, then calibrate the angular scalar, then
-        // the linear scalar. To calibrate the angular scalar, spin the robot by
-        // multiple rotations (eg. 10) to get a precise error, then set the scalar
-        // to the inverse of the error. Remember that the angle wraps from -180 to
-        // 180 degrees, so for example, if after 10 rotations counterclockwise
-        // (positive rotation), the sensor reports -15 degrees, the required scalar
-        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
-        // robot a known distance and measure the error; do this multiple times at
-        // multiple speeds to get an average, then set the linear scalar to the
-        // inverse of the error. For example, if you move the robot 100 inches and
-        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
-        public double linearScalar = 1.0;
-        public double angularScalar = 1.0;
-    }
-
     public static Params PARAMS = new Params();
+    private final DownsampledWriter estimatedPoseWriter = new DownsampledWriter("ESTIMATED_POSE", 50_000_000);
     public SparkFunOTOSCorrected otos;
     private Pose2d lastOtosPose = pose;
 
-    private final DownsampledWriter estimatedPoseWriter = new DownsampledWriter("ESTIMATED_POSE", 50_000_000);
-
     public SparkFunOTOSDrive(HardwareMap hardwareMap, Pose2d pose) {
         super(hardwareMap, pose);
-        FlightRecorder.write("OTOS_PARAMS",PARAMS);
-        otos = hardwareMap.get(SparkFunOTOSCorrected.class,"sensor_otos");
+        FlightRecorder.write("OTOS_PARAMS", PARAMS);
+        otos = hardwareMap.get(SparkFunOTOSCorrected.class, "sensor_otos");
         // RR localizer note:
         // don't change the units, it will stop Dashboard field view from working properly
         // and might cause various other issues
@@ -125,7 +89,7 @@ public class SparkFunOTOSDrive extends AutoMecanumDrive {
         SparkFunOTOS.Pose2D otosPose = new SparkFunOTOS.Pose2D();
         SparkFunOTOS.Pose2D otosVel = new SparkFunOTOS.Pose2D();
         SparkFunOTOS.Pose2D otosAcc = new SparkFunOTOS.Pose2D();
-        otos.getPosVelAcc(otosPose,otosVel,otosAcc);
+        otos.getPosVelAcc(otosPose, otosVel, otosAcc);
         pose = OTOSPoseToRRPose(otosPose);
         lastOtosPose = pose;
 
@@ -139,6 +103,42 @@ public class SparkFunOTOSDrive extends AutoMecanumDrive {
 
         // RR localizer note:
         // OTOS velocity units happen to be identical to Roadrunners, so we don't need any conversion!
-        return new PoseVelocity2d(new Vector2d(otosVel.x, otosVel.y),otosVel.h);
+        return new PoseVelocity2d(new Vector2d(otosVel.x, otosVel.y), otosVel.h);
+    }
+
+    public static class Params {
+        // Assuming you've mounted your sensor to a robot and it's not centered,
+        // you can specify the offset for the sensor relative to the center of the
+        // robot. The units default to inches and degrees, but if you want to use
+        // different units, specify them before setting the offset! Note that as of
+        // firmware version 1.0, these values will be lost after a power cycle, so
+        // you will need to set them each time you power up the sensor. For example, if
+        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
+        // forward (positive Y) of the center of the robot, and mounted 90 degrees
+        // clockwise (negative rotation) from the robot's orientation, the offset
+        // would be {-5, 10, -90}. These can be any value, even the angle can be
+        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
+
+        // RR localizer note: These units are inches and radians.
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
+
+        // Here we can set the linear and angular scalars, which can compensate for
+        // scaling issues with the sensor measurements. Note that as of firmware
+        // version 1.0, these values will be lost after a power cycle, so you will
+        // need to set them each time you power up the sensor. They can be any value
+        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
+        // first set both scalars to 1.0, then calibrate the angular scalar, then
+        // the linear scalar. To calibrate the angular scalar, spin the robot by
+        // multiple rotations (eg. 10) to get a precise error, then set the scalar
+        // to the inverse of the error. Remember that the angle wraps from -180 to
+        // 180 degrees, so for example, if after 10 rotations counterclockwise
+        // (positive rotation), the sensor reports -15 degrees, the required scalar
+        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
+        // robot a known distance and measure the error; do this multiple times at
+        // multiple speeds to get an average, then set the linear scalar to the
+        // inverse of the error. For example, if you move the robot 100 inches and
+        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
+        public double linearScalar = 1.0;
+        public double angularScalar = 1.0;
     }
 }
