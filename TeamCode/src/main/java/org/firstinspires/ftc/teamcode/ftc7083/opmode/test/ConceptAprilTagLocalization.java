e962e267745ab94eb151f3eb5dc9f0313ab0179a
Roy Brabson <rbrabson@gmail.com>
2024-11-11 14:46:09 -0500

    april tag localization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/opmode/test/ConceptAprilTagLocalization.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/opmode/test/ConceptAprilTagLocalization.java
@@ -41,14 +41,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Position;
-import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.ftc7083.subsystem.Webcam;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -73,73 +70,22 @@ import java.util.List;
 @TeleOp(name = "Concept: AprilTag Localization", group = "tests")
 //@Disabled
 public class ConceptAprilTagLocalization extends LinearOpMode {
-
-    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
-
-    /**
-     * Variables to store the position and orientation of the camera on the robot. Setting these
-     * values requires a definition of the axes of the camera and robot:
-     *
-     * Camera axes:
-     * Origin location: Center of the lens
-     * Axes orientation: +x right, +y down, +z forward (from camera's perspective)
-     *
-     * Robot axes (this is typical, but you can define this however you want):
-     * Origin location: Center of the robot at field height
-     * Axes orientation: +x right, +y forward, +z upward
-     *
-     * Position:
-     * If all values are zero (no translation), that implies the camera is at the center of the
-     * robot. Suppose your camera is positioned 5 inches to the left, 7 inches forward, and 12
-     * inches above the ground - you would need to set the position to (-5, 7, 12).
-     *
-     * Orientation:
-     * If all values are zero (no rotation), that implies the camera is pointing straight up. In
-     * most cases, you'll need to set the pitch to -90 degrees (rotation about the x-axis), meaning
-     * the camera is horizontal. Use a yaw of 0 if the camera is pointing forwards, +90 degrees if
-     * it's pointing straight left, -90 degrees for straight right, etc. You can also set the roll
-     * to +/-90 degrees if it's vertical, or 180 degrees if it's upside-down.
-     */
-    private final Position cameraPosition = new Position(DistanceUnit.INCH,
-            0, 0, 0, 0);
-    private final YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
-            0, -90, 0, 0);
-
-    // List of pre-calibrated camera resolutions supported by the Logi 920 webcam
-    List<Size> supportedCameraResolutions = Arrays.asList(
-            new Size(640, 480),
-            new Size(640, 360),
-            new Size(800, 448),
-            new Size(800, 600),
-            new Size(864, 480),
-            new Size(1920, 1080)
-    );
-    Size cameraResolution;
-
-    /**
-     * The variable to store our instance of the AprilTag processor.
-     */
-    private AprilTagProcessor aprilTag;
-
-    /**
-     * The variable to store our instance of the vision portal.
-     */
-    private VisionPortal visionPortal;
+    private Webcam webcam;
 
     @Override
     public void runOpMode() {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+        // Enable bulk reads. This is almost always the "correct" answer, and can speed up loop
+        // times. We will be managing the bulk read caches manually, which requires the code
+        // to clear the cache at the start of each loop.
         List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
         for (LynxModule hub : allHubs) {
             hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
         }
 
-        cameraResolution = getCameraResolution();
-        initAprilTag();
-        //FtcDashboard.getInstance().startCameraStream(visionPortal, 0);
-
-        long startLoopTime = System.currentTimeMillis();
+        selectCameraResolution();
+        webcam = new Webcam(hardwareMap, telemetry, Webcam.Location.LEFT);
 
         // Wait for the DS start button to be touched.
         telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
@@ -149,6 +95,7 @@ public class ConceptAprilTagLocalization extends LinearOpMode {
 
         waitForStart();
 
+        long startLoopTime;
         while (opModeIsActive()) {
             startLoopTime = System.currentTimeMillis();
             // Clear the bulk cache for each Lynx module hub. This must be performed once per loop
@@ -158,13 +105,13 @@ public class ConceptAprilTagLocalization extends LinearOpMode {
             }
 
             telemetryAprilTag();
-            telemetry.addData("FPS", visionPortal.getFps());
+            telemetry.addData("FPS", webcam.getFps());
 
             // Save CPU resources; can resume streaming when needed.
             if (gamepad1.dpad_down) {
-                visionPortal.stopStreaming();
+                webcam.stopStreaming();
             } else if (gamepad1.dpad_up) {
-                visionPortal.resumeStreaming();
+                webcam.resumeStreaming();
             }
 
             long elapsedTime = System.currentTimeMillis() - startLoopTime;
@@ -172,43 +119,40 @@ public class ConceptAprilTagLocalization extends LinearOpMode {
 
             telemetry.update();
 
-            // Share the CPU.
-            //sleep(20);
-            idle();
+            // Share the CPU
+            sleep(20);
         }
 
         // Save more CPU resources when camera is no longer needed.
-        visionPortal.close();
+        webcam.close();
 
     }   // end method runOpMode()
 
     /**
-     * Returns the camera resolution to be used for the OpMode.
-     * @return the camera resolution.
+     * Sets the camera resolution to be used for the OpMode.
      */
     @SuppressLint("DefaultLocale")
-    private Size getCameraResolution() {
+    private void selectCameraResolution() {
         boolean wasUp = false, wasDown = false;
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        int sizeNum = 0;
-        Size size = supportedCameraResolutions.get(sizeNum);
+        int resolutionSelection = 0;
         while (!gamepad1.cross && !isStarted()) {
             telemetry.addLine("Select camera resolution. Use Up and Down on D-pad to cycle through choices");
             telemetry.addLine("Press `X` to choose the camera resolution");
 
             if (gamepad1.dpad_down && !wasDown) {
-                sizeNum++;
-                if (sizeNum >= supportedCameraResolutions.size()) {
-                    sizeNum = 0;
+                resolutionSelection++;
+                if (resolutionSelection >= Webcam.SUPPORTED_CAMERA_RESOLUTIONS.size()) {
+                    resolutionSelection = 0;
                 }
             } else if (gamepad1.dpad_up && !wasUp) {
-                sizeNum--;
-                if (sizeNum < 0) {
-                    sizeNum = supportedCameraResolutions.size() - 1;
+                resolutionSelection--;
+                if (resolutionSelection < 0) {
+                    resolutionSelection = Webcam.SUPPORTED_CAMERA_RESOLUTIONS.size() - 1;
                 }
             }
-            size = supportedCameraResolutions.get(sizeNum);
+            Size size = Webcam.SUPPORTED_CAMERA_RESOLUTIONS.get(resolutionSelection);
 
             telemetry.addLine(String.format("Camera resolution: %d x %d", size.getWidth(), size.getHeight()));
             telemetry.update();
@@ -218,87 +162,10 @@ public class ConceptAprilTagLocalization extends LinearOpMode {
 
             sleep(20);
         }
+        Webcam.RESOLUTION_SELECTION = resolutionSelection;
+        Size size = Webcam.SUPPORTED_CAMERA_RESOLUTIONS.get(resolutionSelection);
         telemetry.addData("Selected camera resolution", size);
-
-        return size;
     }
-
-    /**
-     * Initialize the AprilTag processor.
-     */
-    private void initAprilTag() {
-
-        // Create the AprilTag processor.
-        aprilTag = new AprilTagProcessor.Builder()
-
-                // The following default settings are available to un-comment and edit as needed.
-                //.setDrawAxes(false)
-                //.setDrawCubeProjection(false)
-                //.setDrawTagOutline(true)
-                //.setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
-                //.setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
-                //.setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
-                .setCameraPose(cameraPosition, cameraOrientation)
-
-                // == CAMERA CALIBRATION ==
-                // If you do not manually specify calibration parameters, the SDK will attempt
-                // to load a predefined calibration for your camera.
-                //.setLensIntrinsics(578.272, 578.272, 402.145, 221.506)
-                // ... these parameters are fx, fy, cx, cy.
-
-                .build();
-
-        // Adjust Image Decimation to trade-off detection-range for detection-rate.
-        // eg: Some typical detection data using a Logitech C920 WebCam
-        // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
-        // Decimation = 2 ..  Detect 2" Tag from 6  feet away at 22 Frames per second
-        // Decimation = 3 ..  Detect 2" Tag from 4  feet away at 30 Frames Per Second (default)
-        // Decimation = 3 ..  Detect 5" Tag from 10 feet away at 30 Frames Per Second (default)
-        // Note: Decimation can be changed on-the-fly to adapt during a match.
-        //aprilTag.setDecimation(3);
-
-        // Create the vision portal by using a builder.
-        VisionPortal.Builder builder = new VisionPortal.Builder();
-
-        // Set the camera (webcam vs. built-in RC phone camera).
-        if (USE_WEBCAM) {
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
-        } else {
-            builder.setCamera(BuiltinCameraDirection.BACK);
-        }
-
-        builder.setCameraResolution(cameraResolution);
-
-        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableLiveView(true);
-
-        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
-
-        // Choose whether or not LiveView stops if no processors are enabled.
-        // If set "true", monitor shows solid orange screen if no processors enabled.
-        // If set "false", monitor shows camera view without annotations.
-        //builder.setAutoStopLiveView(false);
-
-        // Set and enable the processor.
-        builder.addProcessor(aprilTag);
-
-        // Build the Vision Portal, using the above settings.
-        visionPortal = builder.build();
-
-        // Temporarily stop the LiveView on the RC screen. This DOES NOT affect the ability to get
-        // a camera frame on the Driver Station's "Camera Stream" feature.
-        //
-        // This has no effect if you didn't set up a LiveView.
-        //
-        // Stopping the LiveView is recommended during competition to save CPU resources when
-        // a LiveView is not required for debugging purposes.
-        visionPortal.stopLiveView();
-
-        // Disable or re-enable the aprilTag processor at any time.
-        //visionPortal.setProcessorEnabled(aprilTag, true);
-
-    }   // end method initAprilTag()
 
     /**
      * Add telemetry about AprilTag detections.
@@ -306,7 +173,7 @@ public class ConceptAprilTagLocalization extends LinearOpMode {
     @SuppressLint("DefaultLocale")
     private void telemetryAprilTag() {
 
-        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        List<AprilTagDetection> currentDetections = webcam.getDetections();
         telemetry.addData("# AprilTags Detected", currentDetections.size());
 
         // Step through the list of detections and display info for each one.
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc7083/subsystem/Webcam.java
@@ -1,41 +1,114 @@
 package org.firstinspires.ftc.teamcode.ftc7083.subsystem;
 
+import android.util.Size;
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.stream.CameraStreamServer;
-import org.firstinspires.ftc.teamcode.ftc7083.field.TeamElementLocation;
-import org.firstinspires.ftc.teamcode.ftc7083.vision.VisionProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
- * A vision sensor used to create and obtain data from a vision processor and determine the
- * location of the team prop.
+ * A webcam used to create and obtain data from a vision processor and detect the visible
+ * April Tags.
  */
+@Config
 public class Webcam extends SubsystemBase {
+    // List of pre-calibrated camera resolutions supported by the Logi 920 webcam
+    public static final List<Size> SUPPORTED_CAMERA_RESOLUTIONS = Arrays.asList(
+            new Size(640, 480),
+            new Size(640, 360),
+            new Size(800, 448),
+            new Size(800, 600),
+            new Size(864, 480),
+            new Size(1920, 1080)
+    );
 
-    private final VisionProcessor visionProcessor;
-    private VisionPortal webcamPortal;
+    // Indexes into SUPPORTED_CAMERA_RESOLUTIONS
+    public static final int RESOLUTION_640x480 = 0;
+    public static final int RESOLUTION_640x360 = 1;
+    public static final int RESOLUTION_800x448 = 2;
+    public static final int RESOLUTION_800x600 = 3;
+    public static final int RESOLUTION_864x480 = 4;
+    public static final int RESOLUTION_1920x1080 = 5;
+
+    // Resolution to use for the webcams. This is an index into the SUPPORTED_CAMERA_RESOLUTIONS list.
+    public static int RESOLUTION_SELECTION = RESOLUTION_864x480;
+
+    // Positions and orientations for the webcams on this robot
+    public static Position LEFT_CAMERA_POSITION = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    public static YawPitchRollAngles LEFT_CAMERA_ORIENTATION = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+    public static Position RIGHT_CAMERA_POSITION = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    public static YawPitchRollAngles RIGHT_CAMERA_ORIENTATION = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+
+    private final Location location;
+    private VisionPortal visionPortal;
+    private AprilTagProcessor aprilTag;
 
     /**
      * Creates a new vision sensor for the webcam with the given name.
      *
-     * @param deviceName  name of the webcam
      * @param hardwareMap mapping of all the hardware on the robot
-     * @param telemetry   the telemetry used to provide output on the driver station.
+     * @param telemetry   the telemetry used to provide output on the driver station
+     * @param location    the location of the webcam on the robot
      */
-    public Webcam(@NonNull String deviceName, @NonNull HardwareMap hardwareMap, @NonNull Telemetry telemetry) {
+    public Webcam(@NonNull HardwareMap hardwareMap, @NonNull Telemetry telemetry, @NonNull Location location) {
+        this.location = location;
+        WebcamName webcamName;
+        if (location == Location.LEFT) {
+            webcamName = hardwareMap.get(WebcamName.class, Location.LEFT.webcamName());
+            initAprilTag(webcamName, LEFT_CAMERA_POSITION, LEFT_CAMERA_ORIENTATION);
+        } else {
+            webcamName = hardwareMap.get(WebcamName.class, Location.RIGHT.webcamName());
+            initAprilTag(webcamName, RIGHT_CAMERA_POSITION, RIGHT_CAMERA_ORIENTATION);
+        }
 
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, deviceName);
+        CameraStreamServer.getInstance().setSource(visionPortal);
+    }
 
-        visionProcessor = new VisionProcessor(telemetry);
-        webcamPortal = VisionPortal.easyCreateWithDefaults(webcamName, visionProcessor);
-        FtcDashboard.getInstance().startCameraStream(webcamPortal, 0);
-        CameraStreamServer.getInstance().setSource(webcamPortal);
+    /**
+     * Initialize the AprilTag processor.
+     */
+    private void initAprilTag(WebcamName webcam, Position cameraPosition, YawPitchRollAngles cameraOrientation) {
+        // Get the camera resolution selection
+        Size cameraResolution = SUPPORTED_CAMERA_RESOLUTIONS.get(RESOLUTION_SELECTION);
+
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+
+        // Create the vision portal by using a builder.
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(webcam)
+                .setCameraResolution(cameraResolution)
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
+                .addProcessor(aprilTag)
+                .build();
+
+        // Stopping the LiveView is recommended during competition to save CPU resources when
+        // a LiveView is not required for debugging purposes.
+        visionPortal.stopLiveView();
     }
 
     /**
@@ -43,19 +116,30 @@ public class Webcam extends SubsystemBase {
      * initialized.
      *
      * @return <code>true</code> if the vision sensor is initialized;
-     * <code>false</code> if it is not.
+     *         <code>false</code> if it is not.
      */
     public boolean webcamInitialized() {
-        return webcamPortal != null && webcamPortal.getCameraState() == VisionPortal.CameraState.STREAMING;
+        return visionPortal != null && visionPortal.getCameraState() == VisionPortal.CameraState.STREAMING;
     }
 
     /**
-     * Returns the location of the team element.
+     * Get a list containing the latest detections, which may be stale
+     * i.e. the same as the last time you called this
      *
-     * @return the location of the team element.
+     * @return a list containing the latest detections.
      */
-    public TeamElementLocation getTeamElementLocation() {
-        return visionProcessor.getSelection();
+    public ArrayList<AprilTagDetection> getDetections() {
+        return aprilTag.getDetections();
+    }
+
+    /**
+     * Get the current rate at which frames are passing through the vision portal
+     * (and all processors therein) per second - frames per second.
+     *
+     * @return the current vision frame rate in frames per second
+     */
+    public float getFps() {
+        return visionPortal.getFps();
     }
 
     /**
@@ -63,11 +147,37 @@ public class Webcam extends SubsystemBase {
      * once the webcam portal has been closed.
      */
     public void close() {
-        if (webcamPortal != null && webcamPortal.getCameraState() != VisionPortal.CameraState.CAMERA_DEVICE_CLOSED) {
-            FtcDashboard.getInstance().stopCameraStream();
-            webcamPortal.close();
-            webcamPortal = null;
+        if (visionPortal != null && visionPortal.getCameraState() != VisionPortal.CameraState.CAMERA_DEVICE_CLOSED) {
+            visionPortal.close();
+            visionPortal = null;
         }
+    }
+
+    /**
+     * Stop the streaming session. This is an asynchronous call which does not take effect
+     * immediately.
+     * <p>
+     * Stopping the streaming session is a good way to save computational resources if there may
+     * be long (e.g. 10+ second) periods of match play in which vision processing is not required.
+     * When streaming is stopped, no new image data is acquired from the camera and any attached
+     * {@link VisionProcessor}s will lie dormant until such time as {@link #resumeStreaming()} is called.
+     * <p>
+     * Stopping and starting the stream can take a second or two, and thus is not advised for use
+     * cases where instantaneously enabling/disabling vision processing is required.
+     */
+    public void stopStreaming() {
+        visionPortal.stopStreaming();
+    }
+
+    /**
+     * Resume the streaming session if previously stopped by {@link #stopStreaming()}. This is
+     * an asynchronous call which does not take effect immediately. If you call {@link #stopStreaming()}
+     * before the operation is complete, it will SYNCHRONOUSLY await completion of the resume command.
+     * <p>
+     * See notes about use case on {@link #stopStreaming()}
+     */
+    public void resumeStreaming() {
+        visionPortal.resumeStreaming();
     }
 
     /**
@@ -79,8 +189,36 @@ public class Webcam extends SubsystemBase {
     @Override
     public String toString() {
         return "Webcam{" +
-                "visionProcessor=" + visionProcessor +
-                ", webcamPortal=" + webcamPortal +
+                ", visionPortal=" + visionPortal +
+                ", location=" + location +
                 '}';
+    }
+
+    /**
+     * Position of the webcam on the robot
+     */
+    public enum Location {
+        LEFT("Webcam 1"),
+        RIGHT("Webcam 2");
+
+        private final String webcamName;
+
+        /**
+         * Instantiates a new enum with the given webcam name.
+         *
+         * @param webcamName the name of the webcam as configured on the REV control hub
+         */
+        Location(String webcamName) {
+            this.webcamName = webcamName;
+        }
+
+        /**
+         * Gets the name of the webcam
+         *
+         * @return the name of the webcam as configured on the REV control hub
+         */
+        public String webcamName() {
+            return webcamName;
+        }
     }
 }
