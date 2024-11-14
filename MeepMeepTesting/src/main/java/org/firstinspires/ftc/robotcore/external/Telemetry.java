f9181bd50e88802126200c16e632b85366405717
Roy Brabson <rbrabson@gmail.com>
2024-11-14 03:56:29 -0500

    meepmeep

--- MeepMeepTesting/src/main/java/org/firstinspires/ftc/robotcore/external/Telemetry.java
+++ MeepMeepTesting/src/main/java/org/firstinspires/ftc/robotcore/external/Telemetry.java
@@ -5,4 +5,8 @@ public class Telemetry {
     public void addData(String caption, Object value) {
         System.out.println(caption + ": " + value);
     }
+
+    public void addLine(String lineCaption) {
+        System.out.println(lineCaption);
+    }
 }
